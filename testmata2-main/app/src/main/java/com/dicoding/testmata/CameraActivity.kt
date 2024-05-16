86ec15ebd4eb62d38740e2ecd1c4498b134f93a9
jihanfebri <jihanfebri424@gmail.com>
2024-05-16 15:53:48 +0700

    update

---
+++ testmata2-main/app/src/main/java/com/dicoding/testmata/CameraActivity.kt
@@ -0,0 +1,154 @@
+package com.dicoding.testmata
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.OrientationEventListener
+import android.view.Surface
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import android.graphics.drawable.ColorDrawable
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
+import android.content.res.Configuration
+import com.dicoding.testmata.databinding.ActivityCameraBinding
+
+class CameraActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCameraBinding
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    private var imageCapture: ImageCapture? = null
+
+    private val orientationEventListener by lazy {
+        object : OrientationEventListener(this) {
+            override fun onOrientationChanged(orientation: Int) {
+                if (orientation == ORIENTATION_UNKNOWN) {
+                    return
+                }
+                val rotation = when (orientation) {
+                    in 45 until 135 -> Surface.ROTATION_270
+                    in 135 until 225 -> Surface.ROTATION_180
+                    in 225 until 315 -> Surface.ROTATION_90
+                    else -> Surface.ROTATION_0
+                }
+                imageCapture?.targetRotation = rotation
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        orientationEventListener.enable()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        orientationEventListener.disable()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Check if dark mode is enabled
+        val isDarkMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+
+        // Set the app bar color based on the mode
+        val appBarColor = if (isDarkMode) {
+            ContextCompat.getColor(this, R.color.primary_dark) // warna untuk dark mode
+        } else {
+            ContextCompat.getColor(this, R.color.primary_light) // warna untuk light mode
+        }
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(appBarColor))
+
+        // Ensure the action bar title text color is white
+        supportActionBar?.setDisplayShowTitleEnabled(true)
+        supportActionBar?.setTitle(HtmlCompat.fromHtml("<font color=\"#FFFFFF\">Camera</font>", HtmlCompat.FROM_HTML_MODE_LEGACY))
+
+        binding.btnCapture.setOnClickListener { takePhoto() }
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startCamera()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            } catch (exc: Exception){
+                Toast.makeText(
+                    this@CameraActivity,
+                    "Gagal memunculkan kamera.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.e(TAG, "startCamera: ${exc.message}")
+
+
+            }
+        }, ContextCompat.getMainExecutor(this ))
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture?: return
+
+        val photoFile = createCustomTempFile(application)
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val intent = Intent()
+                    intent.putExtra(EXTRA_CAMERAX_IMAGE, outputFileResults.savedUri.toString())
+                    setResult(CAMERAX_RESULT, intent)
+                    finish()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        this@CameraActivity,
+                        "Gagal mengambil gambar.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.e(TAG, "onError: ${exception    .message}")
+                }
+
+            }
+        )
+    }
+
+    companion object {
+        private const val TAG = "CameraActivity"
+        const val EXTRA_CAMERAX_IMAGE = "CameraX Image"
+        const val CAMERAX_RESULT = 200
+    }
+}
